76dcb54f992443e265b89118aed225c1692d2d3a
Diego Caucota <diegocaucota@gmail.com>
2024-09-26 11:21:42 -0300

    add: folder structure

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,38 @@
+import 'package:go_router/go_router.dart';
+import 'package:music_app/features/artist/ui/screen/artist_screen.dart';
+import 'package:music_app/features/home/ui/screen/home_screen.dart';
+import 'package:music_app/features/playslist/ui/screen/playslist_screen.dart';
+import 'package:music_app/features/splash/ui/screen/splahs_screen.dart';
+
+final appRoutes = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: SplashScreen.name,
+      builder: (context, state) => const SplashScreen(),
+      routes: [
+        GoRoute(
+          path: HomeScreen.name,
+          builder: (context, state) => const HomeScreen(),
+          routes: [
+            GoRoute(
+              name: PlaysListScreen.name,
+              path: PlaysListScreen.name,
+              builder: (context, state) {
+                final id = int.parse(state.extra.toString());
+                return PlaysListScreen(id: id);
+              },
+            ),
+            GoRoute(
+              name: ArtistScreen.name,
+              path: ArtistScreen.name,
+              builder: (context, state) {
+                return const ArtistScreen();
+              },
+            ),
+          ],
+        )
+      ],
+    ),
+  ],
+);
