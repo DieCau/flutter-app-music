dcddc3e352bbc711928bac2490a28653b5bc2c93
Diego Caucota <diegocaucota@gmail.com>
2024-09-28 11:56:50 -0300

    add: feature artist data all folders

---
+++ lib/features/home/data/local/datasource/datasource_local_db.dart
@@ -0,0 +1,31 @@
+import 'package:dio/dio.dart';
+import 'package:music_app/shared/database_service/dao/music_dao.dart';
+import 'package:music_app/shared/database_service/models/music_localdb.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+
+class DatasourceLocalDb {
+  final musicDao = MusicDao();
+  final dio = Dio();
+
+  Future<List<MusicLocalDb>> getAllMusicDownload() async {
+    final response = await musicDao.getAllMusic();
+
+    return response;
+  }
+
+  Future<void> inserMusic({required MusicLocalDb music}) async {
+    try {
+      final String nameFileUpdate = "${music.title.replaceAll(' ', '_')}.mp3";
+      final String nameImageFileUpdate = "${music.title.replaceAll(' ', '_')}.jpg";
+      final dir = await getApplicationDocumentsDirectory();
+      final filePath = join(dir.path, nameFileUpdate);
+      final fileImagePath = join(dir.path, nameImageFileUpdate);
+      await dio.download(music.urlPath, filePath);
+      await dio.download(music.imagePath, fileImagePath);
+      await musicDao.insertMusic(music.copyWith(urlPath: filePath, imagePath: fileImagePath));
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+}
---
+++ lib/features/home/data/memory/datasource_memory_db_home.dart
@@ -0,0 +1,23 @@
+import 'package:music_app/features/home/domain/entites/favority_entity/your_favorite_music_entity.dart';
+
+class DatasourceMemorydbHome {
+  Future<void> addFavoriteMusic({required YourFavoriteMusicEntity model}) async {
+    if (!musicProvider.contains(model)) {
+      musicProvider.add(model);
+    } else {}
+  }
+
+  Future<void> remodeTrack({required int id}) async {
+    try {
+      musicProvider.removeWhere((element) => element.id == id);
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<List<YourFavoriteMusicEntity>> fetchAllFavoriteMusic() async {
+    return musicProvider;
+  }
+}
+
+final List<YourFavoriteMusicEntity> musicProvider = [];
