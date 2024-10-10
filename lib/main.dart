71dfba8901dd48febb49fccc1e3e3725f100c9fd
Diego Caucota <diegocaucota@gmail.com>
2024-10-09 23:31:07 -0300

    add: comments main

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:app_music/shared/database_service/database_services.dart';
 import 'package:flutter/material.dart';
 import 'package:app_music/main/my_app.dart';
 
+// Start app
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await DatabaseService.instance.init();
